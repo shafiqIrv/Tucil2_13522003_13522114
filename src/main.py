33db3acec4ba43551a8a514423e77a0c4f9d7838
XAVAX ​ <mdavaf17@gmail.com>
2024-03-19 11:14:21 +0700

    feat: final 1

--- src/main.py
+++ src/main.py
@@ -7,12 +7,14 @@ import ctypes
 ctypes.windll.shcore.SetProcessDpiAwareness(0)
 import time
 
+
 # DATA
 controlPoints = []
 bezierPoints = []
 lc = []
 nIter = None
 N = None
+isProcessing = False
 
 
 
@@ -39,8 +41,42 @@ def bezier_curve(control_points, n):
     return curve
 
 
+def quadraticBezier(ctrl1, ctrl2, ctrl3, limIter, currIter=0):
+    if (currIter < limIter):
+        midP1 = getMidPoint(ctrl1, ctrl2)
+        midP2 = getMidPoint(ctrl2, ctrl3)
 
-def PopulateBeizerPoints(controlPoints, limIter, currIter=0):
+        midP3 = getMidPoint(midP1, midP2)
+
+        # Left segment
+        quadraticBezier(ctrl1, midP1, midP3, limIter, currIter+1)
+
+        bezierPoints.append(midP3)
+        
+        # Right segment
+        quadraticBezier(midP3, midP2, ctrl3, limIter, currIter+1)
+
+
+def animateQuadraticBezier(ctrl1, ctrl2, ctrl3, limIter, currIter=0):
+    if (currIter < limIter):
+        midP1 = getMidPoint(ctrl1, ctrl2)
+        midP2 = getMidPoint(ctrl2, ctrl3)
+
+        midP3 = getMidPoint(midP1, midP2)
+
+        ax.plot([ctrl1[0], midP1[0], midP2[0], ctrl3[0]], [ctrl1[1], midP1[1], midP2[1], ctrl3[1]], color=lc[currIter], marker='o', linestyle='-')
+        
+        animateQuadraticBezier(ctrl1, midP1, midP3, limIter, currIter+1)
+
+        bezierPoints.append(midP3)
+        
+        animateQuadraticBezier(midP3, midP2, ctrl3, limIter, currIter+1)
+
+        fig.canvas.draw_idle()
+        fig.canvas.start_event_loop(0.00005)
+
+
+def NthBezier(controlPoints, limIter, currIter=0):
     if (currIter < limIter):
         midPoints = controlPoints.copy()
         
@@ -61,13 +97,15 @@ def PopulateBeizerPoints(controlPoints, limIter, currIter=0):
         right = right[::-1]
 
         # LEFT SEGMENT
-        PopulateBeizerPoints(left, nIter, currIter+1)
+        NthBezier(left, nIter, currIter+1)
+
         bezierPoints.append(midPoints[0])
+
         # RIGHT SEGMENT
-        PopulateBeizerPoints(right, nIter, currIter+1)
+        NthBezier(right, nIter, currIter+1)
 
 
-def AnimateBezierPoint(controlPoints, limIter, currIter=0):
+def animateNthBezier(controlPoints, limIter, currIter=0):
     if (currIter < limIter):
         midPoints = controlPoints.copy()
         
@@ -94,29 +132,30 @@ def AnimateBezierPoint(controlPoints, limIter, currIter=0):
         right = right[::-1]
 
         # LEFT SEGMENT
-        AnimateBezierPoint(left, nIter, currIter+1)
+        animateNthBezier(left, nIter, currIter+1)
+
         bezierPoints.append(midPoints[0])
+        
         # RIGHT SEGMENT
-        AnimateBezierPoint(right, nIter, currIter+1)
+        animateNthBezier(right, nIter, currIter+1)
         fig.canvas.draw_idle()
-        fig.canvas.start_event_loop(0.05)
+        fig.canvas.start_event_loop(0.00005)
 
-
-def changeLabelVar(text):
-    EntryPoint_var.set(text)
 
 def clickPoint(event):
-    if event.button == 1:  # Left mouse button click
-        x = float("{:.3f}".format(event.xdata))
-        y = float("{:.3f}".format(event.ydata))
-        if ((x, y) not in controlPoints):
-            controlPoints.append((x, y))
-            show_plot()
-            update_control_points_list()
+    if not isProcessing:
+        if event.button == 1:  # Left mouse button click
+            x = float("{:.3f}".format(event.xdata))
+            y = float("{:.3f}".format(event.ydata))
+            if ((x, y) not in controlPoints):
+                controlPoints.append((x, y))
+                show_plot()
+                update_control_points_list()
+
 
 def show_plot():
     ax.clear()
-    changeLabelVar(str(len(controlPoints)))
+    EntryPoint.config(text=(str(len(controlPoints))))
     for point in controlPoints:
         ax.plot(point[0], point[1], 'ro')
         ax.text(point[0], point[1], f'({point[0]}, {point[1]})', fontsize=8)
@@ -126,12 +165,12 @@ def show_plot():
     ax.set_ylabel('Y')
     fig.canvas.draw()
 
+
 def update_control_points_list():
     control_points_list.delete(0, tk.END)
     for i, point in enumerate(controlPoints, start=1):
         control_points_list.insert(tk.END, f"{i}) {point[0]}, {point[1]}")
     
-
 
 def addTypedPoint():
     try:
@@ -140,124 +179,188 @@ def addTypedPoint():
             controlPoints.append((x, y))
             show_plot()
             update_control_points_list()
-            EntryAddPoint.delete(0, tk.END)  # Clear the entry after adding the point
+            EntryAddPoint.delete(0, tk.END)
     except ValueError:
-        # If the input is not in the correct format
-        tk.messagebox.showerror("Error", "Invalid input format. Please enter coordinates in the format 'x, y'.")
+        tk.messagebox.showerror("Error", "Invalid input format. Please enter coordinates in the format 'X Y'.")
+
+
+def clearControlPoints():
+    controlPoints.clear()
+    update_control_points_list()
+    show_plot()
 
 
 def runDNC():
+    global isProcessing
+    isProcessing = True
+
+    BtnAddPoint.config(state='disabled')
     bezierPoints.clear()
     global nIter, lc
-    nIter = int(EntryIteration.get())
-    lc = [(np.random.random(), np.random.random(), np.random.random()) for i in range(nIter+1)]
-
-    for i in range(nIter):
-        bezierPoints.append(controlPoints[0])
-        PopulateBeizerPoints(controlPoints, i+1)
-        bezierPoints.append(controlPoints[-1])
-
-        if i != nIter-1:
-            bezierPoints.clear()
     
-    ax.clear()
+    nIter = int(EntryIteration.get())
 
-    ax.plot([point[0] for point in controlPoints], [point[1] for point in controlPoints], 'ko-', markerfacecolor='red')
+    if (nIter > 0 and len(controlPoints) >= 3):
+        lc = [(np.random.random(), np.random.random(), np.random.random()) for i in range(nIter+1)]
 
-    for point in controlPoints:
-        ax.text(point[0], point[1], f'({point[0]}, {point[1]})', fontsize=8)
 
-    ax.plot([point[0] for point in bezierPoints], [point[1] for point in bezierPoints], 'r.-')
-    fig.canvas.draw()
+        start = time.time()
+
+        if (len(controlPoints) == 3):
+            bezierPoints.append(controlPoints[0])
+            quadraticBezier(controlPoints[0], controlPoints[1], controlPoints[2], nIter)
+            bezierPoints.append(controlPoints[-1])
+        else:
+            bezierPoints.append(controlPoints[0])
+            NthBezier(controlPoints, nIter)
+            bezierPoints.append(controlPoints[-1])
+        
+        end = time.time()
+        LabelDNCTimeVal.config(text=f"{(end - start):.10f} s")
+        
+        ax.clear()
+
+        ax.plot([point[0] for point in controlPoints], [point[1] for point in controlPoints], 'ko-', markerfacecolor='red')
+
+        for point in controlPoints:
+            ax.text(point[0], point[1], f'({point[0]}, {point[1]})', fontsize=8)
+
+        ax.plot([point[0] for point in bezierPoints], [point[1] for point in bezierPoints], 'r.-')
+        for point in bezierPoints:
+            ax.text(point[0], point[1], f'({point[0]:.3f}, {point[1]:.3f})', fontsize=8)
+
+        ax.set_title('Click to add points')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        fig.canvas.draw()
+
+    BtnAddPoint.config(state='normal')
+
+    isProcessing = False
+
 
 def animateDNC():
+    global isProcessing
+    isProcessing = True
+
+    BtnAddPoint.config(state='disabled')
     bezierPoints.clear()
     global nIter, lc
     nIter = int(EntryIteration.get())
-    lc = [(np.random.random(), np.random.random(), np.random.random()) for i in range(nIter+1)]
 
-    ax.clear()
+    if (nIter > 0 and len(controlPoints) >= 3):
+        lc = [(np.random.random(), np.random.random(), np.random.random()) for i in range(nIter+1)]
 
-    ax.plot([point[0] for point in controlPoints], [point[1] for point in controlPoints], 'ko-', markerfacecolor='red')
+        ax.clear()
 
-    for point in controlPoints:
-        ax.text(point[0], point[1], f'({point[0]}, {point[1]})', fontsize=8)
+        ax.plot([point[0] for point in controlPoints], [point[1] for point in controlPoints], 'ko-', markerfacecolor='red')
 
-    for i in range(nIter):
-        bezierPoints.append(controlPoints[0])
-        AnimateBezierPoint(controlPoints, i+1)
-        bezierPoints.append(controlPoints[-1])
+        for point in controlPoints:
+            ax.text(point[0], point[1], f'({point[0]}, {point[1]})', fontsize=8)
+        
+        if (len(controlPoints) == 3):
+            for i in range(nIter):
+                bezierPoints.append(controlPoints[0])
+                animateQuadraticBezier(controlPoints[0], controlPoints[1], controlPoints[2], i+1)
+                bezierPoints.append(controlPoints[-1])
 
-        if i != nIter-1:
-            bezierPoints.clear()
+                if i != nIter-1:
+                    bezierPoints.clear()
+        else:
+            for i in range(nIter):
+                bezierPoints.append(controlPoints[0])
+                animateNthBezier(controlPoints, i+1)
+                bezierPoints.append(controlPoints[-1])
+
+                if i != nIter-1:
+                    bezierPoints.clear()
+        
+        ax.plot([point[0] for point in bezierPoints], [point[1] for point in bezierPoints], 'ro-')
+        for point in bezierPoints:
+            ax.text(point[0], point[1], f'({point[0]:.3f}, {point[1]:.3f})', fontsize=8)
+
+        ax.set_title('Click to add points')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        fig.canvas.draw()
     
-    ax.plot([point[0] for point in bezierPoints], [point[1] for point in bezierPoints], 'ro-')
-    fig.canvas.draw()
+    
+    BtnAddPoint.config(state='normal')
 
-
+    isProcessing = False
 
 def runBF():
+    global isProcessing
+    isProcessing = True
+
+    BtnAddPoint.config(state='disabled')
     bezierPoints.clear()
     ax.clear()
 
-    N = int(EntryBFBezierPoint.get())
-    time_gap = 5 / N
-    start = time.time()
-    result = bezier_curve(controlPoints, N)
-    end = time.time()
+    N = int(EntryBFIteration.get())
 
-    LabelBFTimeVal.config(text=f"{end - start} s")
+    if (N > 0 and len(controlPoints) >= 3):
+        start = time.time()
+        result = bezier_curve(controlPoints, N)
+        end = time.time()
 
-    # Plot points
-    ax.plot([point[0] for point in result], [point[1] for point in result], 'ko-', markerfacecolor='red')
-
-    # Annotate each point with its coordinates formatted to three decimal places
-    for point in result:
-        ax.text(point[0], point[1], f'({point[0]:.3f}, {point[1]:.3f})', fontsize=8)
+        LabelBFTimeVal.config(text=f"{(end - start):.10f} s")
 
 
+        # Plot points
+        ax.plot([point[0] for point in result], [point[1] for point in result], 'ro-', markerfacecolor='red')
 
-    # for i in range(len(result) - 1):
-    #     ax.plot(
-    #         [result[i][0], result[i + 1][0]],
-    #         [result[i][1], result[i + 1][1]],
-    #         marker="o",
-    #         linestyle="-",
-    #         color="b",
-    #     )
+        # Annotate each point with its coordinates formatted to three decimal places
+        for point in result:
+            ax.text(point[0], point[1], f'({point[0]:.3f}, {point[1]:.3f})', fontsize=8)
 
         
-    #     ax.text(result[i][0], result[i][1], f'({result[i][0]:.3f}, {result[i][1]:.3f})', fontsize=8)
-    #     fig.canvas.draw_idle()
-    #     fig.canvas.start_event_loop(time_gap)
-    
-    # ax.text(result[-1][0], result[-1][1], f'({result[-1][0]:.3f}, {result[-1][1]:.3f})', fontsize=8)
-    fig.canvas.draw()
+        ax.set_title('Click to add points')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
 
+        fig.canvas.draw()
+    
+    BtnAddPoint.config(state='normal')
+
+    isProcessing = False
 
 
 def animateBF():
+    global isProcessing
+    isProcessing = True
+
+    BtnAddPoint.config(state='disabled')
     bezierPoints.clear()
     ax.clear()
 
-    N = int(EntryBFBezierPoint.get())
-    time_gap = 5 / N
-    result = bezier_curve(controlPoints, N)
+    N = int(EntryBFIteration.get())
 
-    for i in range(len(result) - 1):
-        ax.plot(
-            [result[i][0], result[i + 1][0]],
-            [result[i][1], result[i + 1][1]],
-            'ro-'
-        )
+    if (N > 0 and len(controlPoints) >= 3):
+        result = bezier_curve(controlPoints, N)
 
+        for i in range(len(result) - 1):
+            ax.plot(
+                [result[i][0], result[i + 1][0]],
+                [result[i][1], result[i + 1][1]],
+                'ro-'
+            )
+
+            
+            ax.text(result[i][0], result[i][1], f'({result[i][0]:.3f}, {result[i][1]:.3f})', fontsize=8)
+            fig.canvas.draw_idle()
+            fig.canvas.start_event_loop(0.05)
         
-        ax.text(result[i][0], result[i][1], f'({result[i][0]:.3f}, {result[i][1]:.3f})', fontsize=8)
-        fig.canvas.draw_idle()
-        fig.canvas.start_event_loop(time_gap)
-    
-    ax.text(result[-1][0], result[-1][1], f'({result[-1][0]:.3f}, {result[-1][1]:.3f})', fontsize=8)
-    fig.canvas.draw()
+        ax.text(result[-1][0], result[-1][1], f'({result[-1][0]:.3f}, {result[-1][1]:.3f})', fontsize=8)
+
+        ax.set_title('Click to add points')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        fig.canvas.draw()
+
+
+    BtnAddPoint.config(state='normal')
+    isProcessing = False
 
 
 # Initialize tkinter
@@ -275,10 +378,6 @@ fig.canvas.mpl_connect('button_press_event', clickPoint)
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().place(relx=0.35, rely=0.0)
 
-# Tkinter Data
-EntryPoint_var = tk.StringVar()
-DNCTime = tk.StringVar()
-
 # TKinter Widgets
 heading1 = tk.Label(root, text="Bezier Curve Simulator", width=30, bg="#26242f", fg="#FFF", font=(custFont, 15))
 heading1.place(x=0, y=0)
@@ -286,7 +385,7 @@ heading1.place(x=0, y=0)
 LabelEntryPoint = tk.Label(root, text="Number of point: ", font=(custFont, 13))
 LabelEntryPoint.place(x=0, y=50)
 
-EntryPoint = tk.Label(root, textvariable=EntryPoint_var, font=(custFont, 13))
+EntryPoint = tk.Label(root, text="-", font=(custFont, 13))
 EntryPoint.place(x=150, y=50)
 
 # Control Points Listbox
@@ -308,6 +407,9 @@ EntryAddPoint.place(x=120, y=300)
 
 BtnAddPoint = tk.Button(root, text="add", font=(custFont, 10), command=addTypedPoint)
 BtnAddPoint.place(x=250, y=330)
+
+BtnClearControlPoint = tk.Button(root, text="clear", font=(custFont, 10), command=clearControlPoints)
+BtnClearControlPoint.place(x=250, y=250)
 
 
 # DnC
@@ -340,11 +442,11 @@ LabelBF = tk.Label(root, text="Brute Force", font=(custFont, 13))
 LabelBF.place(x=720, y=500)
 
 # Entry N Point
-LabelEntryBFBezierPoint = tk.Label(root, text="Number of Bezier Point: ", font=(custFont, 13))
-LabelEntryBFBezierPoint.place(x=650, y=530)
+LabelEntryBFIteration = tk.Label(root, text="Number of Iteration: ", font=(custFont, 13))
+LabelEntryBFIteration.place(x=650, y=530)
 
-EntryBFBezierPoint = tk.Entry(root, width=5, font=(custFont, 12))
-EntryBFBezierPoint.place(x=840, y=530)
+EntryBFIteration = tk.Entry(root, width=5, font=(custFont, 12))
+EntryBFIteration.place(x=840, y=530)
 
 BtnRunBF = tk.Button(root, text="Run", font=(custFont, 10), command=runBF)
 BtnRunBF.place(x=750, y=570)
@@ -354,7 +456,6 @@ LabelBFTime.place(x=650, y=600)
 
 LabelBFTimeVal = tk.Label(root, text="-", font=(custFont, 11))
 LabelBFTimeVal.place(x=770, y=600)
-
 
 BtnAnimateBF = tk.Button(root, text="Animate", font=(custFont, 10), command=animateBF)
 BtnAnimateBF.place(x=740, y=630)
